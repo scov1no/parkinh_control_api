e6f8952a96e91c8a4eb715f3b45a42f3c4df7ae5
DevEddi <scovinowork@gmail.com>
2022-02-12 09:26:21 -0300

    first commit

---
+++ src/test/java/com/api/parkinhcontrol/ParkinhControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parkinhcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinhControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
